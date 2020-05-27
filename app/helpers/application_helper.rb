da0e3b546f7b1862ba27151b6b62a88aa61ab9ae
HoangTuan <tuan.hm.bn@gmail.com>
2020-05-27 09:28:18 +0700

    add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Tech Vision Technology Review"
+    base_title = "Tech Vision - Technology Reviews"
     if page_title.empty?
       base_title
     else
