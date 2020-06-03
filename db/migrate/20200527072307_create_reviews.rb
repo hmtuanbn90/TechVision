3a40ff1bebe9116e8d5cc9f4cc1eb9756b37fa1f
tung <hoangvantung.it@gmail.com>
2020-06-03 09:52:31 +0700

    home-and-search

--- db/migrate/20200527072307_create_reviews.rb
+++ db/migrate/20200527072307_create_reviews.rb
@@ -4,6 +4,10 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.text :tittle
       t.text :content
       t.text :about
+      t.text :imager
+      t.integer :view
+      t.boolean :hot
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200527072632_create_hastags.rb
+++ db/migrate/20200527072632_create_hastags.rb
@@ -2,6 +2,8 @@ class CreateHastags < ActiveRecord::Migration[6.0]
   def change
     create_table :hastags do |t|
       t.string :name
+      t.references :review, null: false, foreign_key: true
+      t.references :topic, null: false, foreign_key: true
 
       t.timestamps
     end
