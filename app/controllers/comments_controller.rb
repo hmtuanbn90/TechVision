74bc64e1b2c2509c8f88e64c3205fb748e540bd4
HoangTuan <tuan.hm.bn@gmail.com>
2020-06-16 10:36:58 +0700

    update like function and fix comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @comment = User.find_by params[:id] .comments.build comment_params
+    @comment = current_user.comments.build comment_params
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.review, notice: 'Created'}
-        format.js   { }
+        format.html { redirect_to @comment.review, notice: t("index.Created")}
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -14,19 +13,22 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.find params[:id]
+    @comment = Comment.find(params[:id])
   end
 
   def destroy
-    @comment = Comment.find params[:id]
+    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to @review if @comment.nil?
-    flash[:success] = "comment deleted"
+    flash[:success] = t("index.Comment deleted")
     redirect_to request.referrer || root_url
   end
 
+
   private
-  def comment_params
-    params.require(:comment).permit :content, :review_id, :user_id
-  end
+
+    def comment_params
+      params.require(:comment).permit :content, :review_id, :user_id
+    end
+
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,6 +1,2 @@
 module LikesHelper
-  def pre_like
-    likes = Like.all
-    @pre_like = @review.likes.find {|like| like.user_id == current_user.id}
-  end
 end
