0fef74784bcdeec75396db56e07e9952ad155212
tung <hoangvantung.it@gmail.com>
2020-07-07 09:36:42 +0700

    delegate topic

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,6 +9,7 @@ class Review < ApplicationRecord
   has_many  :bookmarks, dependent: :destroy
   has_many :reports, dependent: :destroy
   has_one_attached :image
+  delegate :name, :to => :topic, :allow_nil => true
   accepts_nested_attributes_for :hashtag_details, allow_destroy: true
   enum appended: { waitting: false, approved: true }
   scope :all_review, -> { order created_at: :desc }
